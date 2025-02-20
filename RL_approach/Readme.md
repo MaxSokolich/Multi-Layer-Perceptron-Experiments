491d411a185383a6ad9f774837b2ab117ee1291f
Aniruddh Puranic <andyruddh@gmail.com>
2025-02-20 16:41:05 -0500

    adding support for action discretization and composable rewards

--- RL_approach/Readme.md
+++ RL_approach/Readme.md
@@ -6,28 +6,32 @@ There is a `requirements.txt` file for the `pip` packages used to run the `ubots
 
 ### Setup
 
-The `ubots_gym.py` implements a _Gymnasium_ class for the _uBots_ simulation, so it can be used with RL libraries like _Stable-Baselines3_.
+The `ubots_env.py` implements a _Gymnasium_ class for the _uBots_ simulation, so it can be used with RL libraries like _Stable-Baselines3_. The base class is `uBotsGym` which implements the original continuous action-space; `uBotsGymDiscrete` inherits the base environment and discretizes the 2-D action-space (i.e., each action is `(freq, angle)`, discretized independently), and then enumerated as a Cartesian product.
 
 Customizations:
 
-1. Choose an on-policy RL (PPO) or off-policy (SAC)
+1. Based on the nature of state-space of the Gym environment:
+    - Continuous-space: Choose an on-policy RL (PPO) or off-policy (SAC, TD3) only. DQN does not work for continuous actions
+    - Discrete-space: Choose models like DQN, PPO, A2C.
 
 2. Set the various environment parameters such as boundary coordinates, sampling time, etc.
 
 3. `_get_goal()` function can generate random goals for each episode or set to fixed locations. Similarly, `__get_init_robot_pos()` can set a random or fixed initial locations for the robots.
 
-4. `_get_reward()` creates a simple dense reward function based on Euclidean distance between robot and corresponding goal positions.
+4. `_get_reward()` function defines custom rewards. See the function for vanilla geometric reward and then reward decomposition.
 
 ## Running experiments
+
+NOTE: 
 
 To train the RL agent, use
 
 ```shell
-$ python ubots_gym.py
+$ python main.py
 ```
 
 and evaluate trained agent with
 
 ```shell
-$ python ubots_gym.py --eval
+$ python main.py --eval
 ```
---
+++ RL_approach/main.py
@@ -0,0 +1,162 @@
+from pathlib import Path
+import argparse
+
+import numpy as np
+
+from ubots_env import *
+
+from stable_baselines3 import PPO, SAC, DQN
+from stable_baselines3.common.logger import configure
+from stable_baselines3.common.env_util import make_vec_env
+
+
+def run_one_episode():
+    env = uBotsGym(N=2)  #, render_mode="human")
+    print("Observation space: ", env.observation_space)
+    print("Action space: ", env.action_space)
+    obs, info = env.reset()
+    for i in range(100):
+        # action = env.action_space.sample()
+        action = (0.1, np.pi / 4)
+        obs, reward, terminated, truncated, info = env.step(action)
+        # env.render()
+    env.close()
+
+
+def make_single_env(env_kwargs):
+    def _init():
+        if ENV_TYPE == 'discrete':
+            env = uBotsGymDiscrete(N=2, **env_kwargs)
+        else:
+            env = uBotsGym(N=2, **env_kwargs)
+        return env
+    return _init
+
+
+def train(alg='ppo', env_kwargs=None):
+    '''RL training function'''
+
+    # Create environment. Multiple parallel/vectorized environments for faster training
+    env = make_vec_env(make_single_env(env_kwargs), n_envs=32)
+
+    if alg == 'ppo':
+        # PPO: on-policy RL
+        policy_kwargs = dict(net_arch=dict(pi=[512, 512], vf=[512, 512]))
+        # policy_kwargs = dict(net_arch=[64, 64, 64, 64])
+        model = PPO("MultiInputPolicy", 
+                    env, 
+                    policy_kwargs=policy_kwargs, 
+                    batch_size=256,
+                    n_epochs=10,
+                    n_steps=2048,
+                    verbose=1)
+
+    elif alg == 'sac':
+        # off-policy RL
+        # policy_kwargs = dict(net_arch=dict(pi=[256, 256], qf=[256, 256]))
+        policy_kwargs = dict(net_arch=[64, 64, 64, 64])
+        model = SAC(
+            "MultiInputPolicy",
+            env,
+            policy_kwargs=policy_kwargs,
+            # use_sde=True,
+            # sde_sample_freq=8,
+            learning_rate=0.0003,
+            learning_starts=1000,
+            batch_size=512,
+            tau=0.05,
+            gamma=0.95,
+            # gradient_steps=1,
+            verbose=1,
+        )
+    else:
+        # PPO: on-policy RL
+        policy_kwargs = dict(net_arch=dict(pi=[512, 512], qf=[512, 512]))
+        # policy_kwargs = dict(net_arch=[64, 64, 64, 64])
+        model = DQN("MultiInputPolicy", 
+                    env, 
+                    policy_kwargs=policy_kwargs, 
+                    earning_starts=400, 
+                    batch_size=256,
+                    tau=1.0, 
+                    gamma=0.99, 
+                    train_freq=4, 
+                    gradient_steps=1, 
+                    verbose=1)
+
+    # log the training params
+    logfile = f"logs/{alg}_ubots_{ENV_TYPE}"
+    tb_logger = configure(logfile, ["stdout", "csv", "tensorboard"])
+    model.set_logger(tb_logger)
+
+    # train the model
+    model.learn(100_000, progress_bar=True)
+
+    model.save(models_dir / f"{alg}_ubots{ENV_TYPE}")
+    del model
+    env.close()
+
+
+def evaluate(alg, env_kwargs, n_trials=3):
+    '''Evaluate the trained RL model'''
+
+    # create single environment for evaluation
+    env = uBotsGymDiscrete(N=2, render_mode="human", **env_kwargs)
+    if alg == 'ppo':
+        ALG = PPO
+    elif alg == 'sac':
+        ALG = SAC
+    else:
+        ALG = DQN
+    
+    # load trained RL model
+    model = ALG.load(models_dir / f"{alg}_ubots{ENV_TYPE}", env=env)
+
+    # run some episodes (trials)
+    for trial in range(n_trials):
+        obs, info = env.reset()
+        done = False
+        while not done:
+            action, _ = model.predict(obs, deterministic=True)
+            obs, reward, terminated, truncated, info = env.step(action)
+            done = terminated or truncated
+            env.render()
+        print(f"Trial: {trial}, Success: {info['is_success']}, # Successes = {info['n_successes']}")
+    env.close()
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--eval",
+                        action="store_true", 
+                        default=False, 
+                        help="Runs the evaluation of a trained model. Default: False (runs RL training by default)")
+    args = parser.parse_args()
+
+    # create directory for saving RL models
+    models_dir = Path("models")
+    models_dir.mkdir(exist_ok=True)
+
+    # create directory for logs
+    log_dir = Path("logs")
+    log_dir.mkdir(exist_ok=True)
+
+    ENV_TYPE = 'discrete' # 'continuous'
+
+    # set environment params
+    env_kwargs = dict(XMIN=-100,
+                 XMAX=100,
+                 YMIN=-100,
+                 YMAX=100,
+                 horizon=200,
+                )
+    
+    # run_one_episode(); exit()
+    alg = ['ppo', 'sac', 'dqn'][0]
+    if not args.eval:
+        # if training
+        train(alg, env_kwargs)
+    else:
+        # if evaluating
+        evaluate(alg, env_kwargs)
